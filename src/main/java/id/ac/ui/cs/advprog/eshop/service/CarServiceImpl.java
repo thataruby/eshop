77921dc67d3a69db1ef0964c58e6635492d9b2ab
thataruby <thata@dfi.id>
2025-02-26 14:01:01 +0700

    fixed file naming errors

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/CarServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/CarServiceImpl.java
@@ -5,7 +5,7 @@ import id.ac.ui.cs.advprog.eshop.repository.CarRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.ArrayList;
-import java.util.Iterator;
+import java.util.Iterator
 import java.util.List;
 
 @Service
